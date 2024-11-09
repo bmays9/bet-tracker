f6f3a7751c636ca12c38d34e593ed4d6c028952b
Barry Mays <barrykmays@gmail.com>
2024-11-09 11:21:27 +0000

    Ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bet-tracker.wsgi
--- bettingtracker/settings.py
+++ bettingtracker/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kxbmtx7m_05yj9fd9ovo&&)*h8e@f_s+gwq!z3i6l60_okt+-r'
+SECRET_KEY = 'pasd123-09-092jeuu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-bmays9-bettracker-5530cl99lsj.ws.codeinstitute-ide.net' 
+    '8000-bmays9-bettracker-5530cl99lsj.ws.codeinstitute-ide.net',
+    '.herokuapp.com'
 ]
 
 
