e6b7cdefb2c9b670693f463e46ac0e9920627e60
Barry Mays <barrykmays@gmail.com>
2024-11-12 19:02:52 +0000

    Add settled bets page and bet status badges

--- bet/urls.py
+++ bet/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.OpenBets.as_view(), name='home'),
+    path('settled/', views.SettledBets.as_view(), name='settled_list'),
 ]
--- bet/views.py
+++ bet/views.py
@@ -17,3 +17,16 @@ class OpenBets(ListView):
     def get_queryset(self):
         queryset = Bet.objects.filter(status=0).order_by("-updated_on")
         return queryset.annotate(line_count=Count('lines')).prefetch_related('lines')
+
+class SettledBets(ListView):
+    """
+    Renders the settled bets page
+    """ 
+         
+    # queryset = Bet.objects.filter(status=0).order_by("updated_on")
+    template_name = "bet/index.html"
+    context_object_name = 'open_bets'
+        
+    def get_queryset(self):
+        queryset = Bet.objects.exclude(status=0).order_by("-updated_on")
+        return queryset.annotate(line_count=Count('lines')).prefetch_related('lines')
